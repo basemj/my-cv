0f8fc8e6ba68e8813946b37eccb9c94531fa413b
basemj <basem.jawahri@gmail.com>
2020-06-03 00:02:54 +0100

    Updates

---
+++ precache-manifest.e4587516d4aebe0e5a7ee09fa3a9d59d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497f0dad3b1bd9257e52c21558348cba",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "cce08ff7fe8c8e5f8771",
+    "url": "/my-cv/static/css/main.dd99394e.chunk.css"
+  },
+  {
+    "revision": "0f2b839e43598c67493d",
+    "url": "/my-cv/static/js/2.a10ab104.chunk.js"
+  },
+  {
+    "revision": "d0f416c0b71d0ec4a4b3180b59613874",
+    "url": "/my-cv/static/js/2.a10ab104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce08ff7fe8c8e5f8771",
+    "url": "/my-cv/static/js/main.a21ce371.chunk.js"
+  },
+  {
+    "revision": "0bdda13005c11294465c",
+    "url": "/my-cv/static/js/runtime-main.b7ac77bb.js"
+  }
+]);
